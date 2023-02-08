55a0894602cf0603f5d920fe97573a3bb7814749
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-02-08 23:57:17 +0545

    setting up error

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,11 @@
+import { ValidationError } from "express-validator";
+
+export class RequestValidationError extends Error {
+  constructor(private errors: ValidationError[]) {
+    super();
+
+    console.log(RequestValidationError.prototype);
+    // only because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,11 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { globalErrorHandler } from "./middlewares/error-handler";
 
 const app: Express = express();
 
-const port = "8000";
+const port = "8080";
 
 // middlewares
 app.use(json());
@@ -17,6 +18,9 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// global error handler
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`);
