7add13e1d709ae60a1cbfc737066f39f7737e84f
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-02-10 01:21:13 +0545

    custom-error abstract class

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const globalErrorHandler = (
   err: Error,
@@ -8,14 +7,8 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeError() });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(err.statusCode).send({
-      errors: err.serializeError(),
-    });
   }
 
   res.status(400).send({
@@ -26,3 +19,4 @@ export const globalErrorHandler = (
     ],
   });
 };
+
